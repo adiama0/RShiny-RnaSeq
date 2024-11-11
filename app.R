6696cef8dd9eed3039326b9873c9c399409eac93
aron Diamond <aronad@scc-wi2.scc.bu.edu>
2024-11-11 13:56:42 -0500

    added function for summarized table

--- app.R
+++ app.R
@@ -10,6 +10,10 @@ library(ggplot2)
 library(colourpicker)
 library(tidyverse)
 library(bigPint) # for dataset download
+library(SummarizedExperiment)
+library(DelayedArray)
+library(dplyr)
+library(tibble)
 
 
 ui <- fluidPage(
@@ -21,12 +25,14 @@ ui <- fluidPage(
       # create sidebar layout
       sidebarLayout(
         sidebarPanel(
-          p("input data set")
+          fileInput("upload", "Load counts data")
         ),
         mainPanel(
           # create 3 tab panels
           tabsetPanel(
-            tabPanel("Sample Summary"),
+            tabPanel("Sample Summary",
+              tableOutput("table_summary")
+            ),
             tabPanel("Data Table"),
             tabPanel("Plots")
           )
@@ -76,6 +82,60 @@ ui <- fluidPage(
 
 server <- function(input, output, session) {
   
+  load_data <- reactive({
+    req(input$upload)  # Require file input
+    data <- read.csv(input$upload$datapath)
+    return(data)
+  })
+  
+  
+  #' create summary table from counts dataframe 
+  #' @param dataf The loaded data frame
+  summarized_table <- function(dataf) {
+    
+    # Extract the column names (e.g., S1.1, S1.2, ...)
+    columns <- colnames(dataf)
+    
+    # Separate the stages and replicates
+    stages <- sapply(strsplit(columns, "\\."), `[`, 1)      # Extracts S1, S2, S3
+    replicates <- sapply(strsplit(columns, "\\."), `[`, 2)  # Extracts 1, 2, 3
+    
+    # Create the coldata for the summary tibble
+    coldata <- data.frame(
+      stage = stages,
+      replicate = replicates,
+      stringsAsFactors = FALSE
+      
+    )
+    
+    # Ensure coldata doesn't have any unwanted rows (e.g., X, NA)
+    coldata <- coldata[complete.cases(coldata), , drop = FALSE]
+    
+    # Create the summary tibble
+    summary_tibble <- tibble(
+      `Column name` = colnames(coldata),
+      Type = sapply(coldata, class),
+      `Mean(sd) or Distinct values` = sapply(coldata, function(col) paste(unique(col), collapse = ", "))
+    )
+    
+    # Add a row for the total number of genes
+    number_of_genes_row <- tibble(
+      `Column name` = "Number of genes",
+      Type = "numeric",
+      `Mean(sd) or Distinct values` = as.character(nrow(dataf))
+    )
+    
+    # Append the new row to the summary_tibble
+    summary_tibble <- bind_rows(summary_tibble, number_of_genes_row)
+    
+    # Return the summary tibble
+    return(summary_tibble)
+  }
+  
+  output$table_summary <- renderTable({
+    req(input$upload)
+    summarized_table(load_data())
+  })
 }
 
 # Run the application
